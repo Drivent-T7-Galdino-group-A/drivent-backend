5552aa13d088ef936e56e1f195bf84d2bcc6521b
Brunna Serafina <brunnaserafina@gmail.com>
2022-12-19 16:07:20 -0300

    docs: updated seed to not add more hotel if they already exist

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -45,282 +45,285 @@ async function main() {
     });
   }
 
-  await prisma.hotel.createMany({
-    data: [
-      {
-        name: "Driven Resort",
-        image: "https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg",
-      },
-      {
-        name: "Driven Palace",
-        image: "https://www.ahstatic.com/photos/5451_ho_00_p_1024x768.jpg",
-      },
-      {
-        name: "Driven World",
-        image:
-          "https://imgcy.trivago.com/c_fill,d_dummy.jpeg,e_sharpen:60,f_auto,h_627,q_auto,w_1200/itemimages/96/95/96959_v6.jpeg",
-      },
-    ],
-  });
+  let hotels = await prisma.hotel.findFirst();
+  if (!hotels) {
+    await prisma.hotel.createMany({
+      data: [
+        {
+          name: "Driven Resort",
+          image: "https://media-cdn.tripadvisor.com/media/photo-s/16/1a/ea/54/hotel-presidente-4s.jpg",
+        },
+        {
+          name: "Driven Palace",
+          image: "https://www.ahstatic.com/photos/5451_ho_00_p_1024x768.jpg",
+        },
+        {
+          name: "Driven World",
+          image:
+            "https://imgcy.trivago.com/c_fill,d_dummy.jpeg,e_sharpen:60,f_auto,h_627,q_auto,w_1200/itemimages/96/95/96959_v6.jpeg",
+        },
+      ],
+    });
 
-  //Driven Resort Rooms
-  await prisma.room.createMany({
-    data: [
-      {
-        name: "101",
-        capacity: 2,
-        hotelId: 1,
-      },
-      {
-        name: "102",
-        capacity: 1,
-        hotelId: 1,
-      },
-      {
-        name: "103",
-        capacity: 2,
-        hotelId: 1,
-      },
-      {
-        name: "104",
-        capacity: 3,
-        hotelId: 1,
-      },
-      {
-        name: "201",
-        capacity: 2,
-        hotelId: 1,
-      },
-      {
-        name: "202",
-        capacity: 1,
-        hotelId: 1,
-      },
-      {
-        name: "203",
-        capacity: 2,
-        hotelId: 1,
-      },
-      {
-        name: "204",
-        capacity: 3,
-        hotelId: 1,
-      },
-      {
-        name: "301",
-        capacity: 2,
-        hotelId: 1,
-      },
-      {
-        name: "302",
-        capacity: 1,
-        hotelId: 1,
-      },
-      {
-        name: "303",
-        capacity: 2,
-        hotelId: 1,
-      },
-      {
-        name: "304",
-        capacity: 3,
-        hotelId: 1,
-      },
-      {
-        name: "401",
-        capacity: 2,
-        hotelId: 1,
-      },
-      {
-        name: "402",
-        capacity: 1,
-        hotelId: 1,
-      },
-      {
-        name: "403",
-        capacity: 2,
-        hotelId: 1,
-      },
-      {
-        name: "404",
-        capacity: 3,
-        hotelId: 1,
-      },
-    ],
-  });
+    //Driven Resort Rooms
+    await prisma.room.createMany({
+      data: [
+        {
+          name: "101",
+          capacity: 2,
+          hotelId: 1,
+        },
+        {
+          name: "102",
+          capacity: 1,
+          hotelId: 1,
+        },
+        {
+          name: "103",
+          capacity: 2,
+          hotelId: 1,
+        },
+        {
+          name: "104",
+          capacity: 3,
+          hotelId: 1,
+        },
+        {
+          name: "201",
+          capacity: 2,
+          hotelId: 1,
+        },
+        {
+          name: "202",
+          capacity: 1,
+          hotelId: 1,
+        },
+        {
+          name: "203",
+          capacity: 2,
+          hotelId: 1,
+        },
+        {
+          name: "204",
+          capacity: 3,
+          hotelId: 1,
+        },
+        {
+          name: "301",
+          capacity: 2,
+          hotelId: 1,
+        },
+        {
+          name: "302",
+          capacity: 1,
+          hotelId: 1,
+        },
+        {
+          name: "303",
+          capacity: 2,
+          hotelId: 1,
+        },
+        {
+          name: "304",
+          capacity: 3,
+          hotelId: 1,
+        },
+        {
+          name: "401",
+          capacity: 2,
+          hotelId: 1,
+        },
+        {
+          name: "402",
+          capacity: 1,
+          hotelId: 1,
+        },
+        {
+          name: "403",
+          capacity: 2,
+          hotelId: 1,
+        },
+        {
+          name: "404",
+          capacity: 3,
+          hotelId: 1,
+        },
+      ],
+    });
 
-  //Driven Palace Rooms
-  await prisma.room.createMany({
-    data: [
-      {
-        name: "101",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "102",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "103",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "104",
-        capacity: 3,
-        hotelId: 2,
-      },
-      {
-        name: "201",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "202",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "203",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "204",
-        capacity: 3,
-        hotelId: 2,
-      },
-      {
-        name: "301",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "302",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "303",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "304",
-        capacity: 3,
-        hotelId: 2,
-      },
-      {
-        name: "401",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "402",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "403",
-        capacity: 2,
-        hotelId: 2,
-      },
-      {
-        name: "404",
-        capacity: 3,
-        hotelId: 2,
-      },
-    ],
-  });
+    //Driven Palace Rooms
+    await prisma.room.createMany({
+      data: [
+        {
+          name: "101",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "102",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "103",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "104",
+          capacity: 3,
+          hotelId: 2,
+        },
+        {
+          name: "201",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "202",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "203",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "204",
+          capacity: 3,
+          hotelId: 2,
+        },
+        {
+          name: "301",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "302",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "303",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "304",
+          capacity: 3,
+          hotelId: 2,
+        },
+        {
+          name: "401",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "402",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "403",
+          capacity: 2,
+          hotelId: 2,
+        },
+        {
+          name: "404",
+          capacity: 3,
+          hotelId: 2,
+        },
+      ],
+    });
 
-  //Driven World Rooms
-  await prisma.room.createMany({
-    data: [
-      {
-        name: "101",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "102",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "103",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "104",
-        capacity: 1,
-        hotelId: 3,
-      },
-      {
-        name: "201",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "202",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "203",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "204",
-        capacity: 1,
-        hotelId: 3,
-      },
-      {
-        name: "301",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "302",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "303",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "304",
-        capacity: 1,
-        hotelId: 3,
-      },
-      {
-        name: "401",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "402",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "403",
-        capacity: 2,
-        hotelId: 3,
-      },
-      {
-        name: "404",
-        capacity: 1,
-        hotelId: 3,
-      },
-    ],
-  });
-
+    //Driven World Rooms
+    await prisma.room.createMany({
+      data: [
+        {
+          name: "101",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "102",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "103",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "104",
+          capacity: 1,
+          hotelId: 3,
+        },
+        {
+          name: "201",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "202",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "203",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "204",
+          capacity: 1,
+          hotelId: 3,
+        },
+        {
+          name: "301",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "302",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "303",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "304",
+          capacity: 1,
+          hotelId: 3,
+        },
+        {
+          name: "401",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "402",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "403",
+          capacity: 2,
+          hotelId: 3,
+        },
+        {
+          name: "404",
+          capacity: 1,
+          hotelId: 3,
+        },
+      ],
+    });
+  }
+  
 }
 
 main()
